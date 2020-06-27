71801414de2b233968f8f8704bbcf3abc0fd5ea7
vanCapelleRob <vancapellerob@gmail.com>
2020-06-27 17:38:30 +0200

    log v1 complete

--- log_installer.sh
+++ log_installer.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-echo $HOME
+#places the log file in /usr/local/bin
+destPath="/usr/local/bin"
+
+sudo wget -O $destPath/log https://raw.githubusercontent.com/vanCapelleRob/scripts/master/log
+sudo chmod 755 $destPath/log
+
+
+#places the .help page in $HOME/.logs/.help
+mkdir $HOME/.logs
+logDir="$HOME/.logs"
+sudo wget -O $logDir/.help  https://raw.githubusercontent.com/vanCapelleRob/scripts/master/help

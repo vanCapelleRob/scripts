dace4530671172c0ab5671a60c44def3041c9fd8
vanCapelleRob <vancapellerob@gmail.com>
2020-06-28 13:42:05 +0200

    Everything made english, /usr/bin/micro changed, working on .QuickNotes dir

--- QuickNotes/qn_installer.sh
+++ QuickNotes/qn_installer.sh
@@ -7,32 +7,32 @@ sudo wget -O $destPath/qn https://raw.githubusercontent.com/vanCapelleRob/script
 sudo chmod 755 $destPath/qn
 
 #on manjaro, $USER changes to root, Ubuntu not.
-is_root(){
-	qnDir="/home/$1/.QuickNotes"
-	mkdir $qnDir
-	sudo chown $1:$1 $qnDir
-}
+#is_root(){
+#	qnDir="/home/$1/.QuickNotes"
+#	mkdir $qnDir
+#	sudo chown $1:$1 $qnDir
+#}
 
-is_user(){
-	qnDir="/home/$USER/.QuickNotes"
-	mkdir $qnDir
-	sudo chown $USER:$USER $qnDir
-}
+#is_user(){
+#	qnDir="/home/$USER/.QuickNotes"
+#	mkdir $qnDir
+#	sudo chown $USER:$USER $qnDir
+#}
 
 #places the .help page in $HOME/.QuickNotes/.help
-if [ ! -z $1 ]; then
-	echo $USER
-	if [ $USER = 'root' ]; then
-		is_root $1
-	else
-		exit
- 		is_user
-	fi
-else
-	is_user
-fi
+#if [ ! -z $1 ]; then
+#	echo $USER
+#	if [ $USER = 'root' ]; then
+#		is_root $1
+#	else
+#		exit
+ #		is_user
+#	fi
+#else
+#	is_user
+#fi
 
-sudo wget -O $qnDir/.help  https://raw.githubusercontent.com/vanCapelleRob/scripts/master/QuickNotes/help
+sudo wget -O $destPath/.help  https://raw.githubusercontent.com/vanCapelleRob/scripts/master/QuickNotes/help
 
 #destroys the installation file
-rm qn_installer*
+#rm qn_installer*

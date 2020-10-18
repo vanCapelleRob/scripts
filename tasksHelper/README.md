67e504537d4435a9816555738cb4ee56c0071494
admin <vancapellerob@gmail.com>
2020-10-18 20:46:13 +0200

    fixed errors

--- tasksHelper/README.md
+++ tasksHelper/README.md
@@ -9,7 +9,7 @@ If you don't have python3 installed on your system do:
 
 ### How to install
 run 
-> 
+> sudo wget -O /home/$USER/tasksHelper_installer https://raw.githubusercontent.com/vanCapelleRob/scripts/master/tasksHelper/tasksHelper_installer.sh && sudo chown $USER:$USER /home/$USER/tasksHelper_installer && sudo chmod 755 /home/$USER/tasksHelper_installer
 
 > ./tasksHelper_installer in your home directory
 
@@ -17,8 +17,9 @@ run
 
 ### What happens
 A file /usr/local/bin/tasks will be made. This file makes it all possible
+A folder /usr/local/bin/.tasksCode will be made. Here is all the code stored to run the program
 
-A folder ~/.tasks will be created. This is where all your notes will be stored.
+A folder ~/.tasks will be created. This is where your task file will be stored.
 
 The tasksHelper_installer(.sh) will be deleted after installation.
 
--- tasksHelper/tasksHelper_installer.sh
+++ tasksHelper/tasksHelper_installer.sh
@@ -3,20 +3,20 @@
 destPath="/usr/local/bin"
 
 sudo wget -O $destPath/tasks https://raw.githubusercontent.com/vanCapelleRob/scripts/master/tasksHelper/tasks
-&& sudo mkdir $destPath/.tasksCode
+sudo mkdir $destPath/.tasksCode
 
-&& sudo chmod 755 $destPath/tasks 
-&& sudo chown $USER:$USER $destPath/tasks 
-# && sudo chmod 755 $destPath/.tasksCode
-&& sudo chown $USER:$USER $destPath/.tasksCode
+sudo chmod 755 $destPath/tasks 
+sudo chown $USER:$USER $destPath/tasks 
+# sudo chmod 755 $destPath/.tasksCode
+sudo chown $USER:$USER $destPath/.tasksCode
 
-&& sudo wget -O $destPath/.tasksCode/App.py
-&& sudo wget -O $destPath/.tasksCode/main.py
-&& sudo wget -O $destPath/.tasksCode/Task.py
+sudo wget -O $destPath/.tasksCode/App.py https://raw.githubusercontent.com/vanCapelleRob/scripts/master/tasksHelper/pyCode/App.py
+sudo wget -O $destPath/.tasksCode/main.py https://raw.githubusercontent.com/vanCapelleRob/scripts/master/tasksHelper/pyCode/main.py
+sudo wget -O $destPath/.tasksCode/Task.py https://raw.githubusercontent.com/vanCapelleRob/scripts/master/tasksHelper/pyCode/Task.py
 
-&& sudo chmod 755 $destPath/.tasksCode/*
-&& sudo chown $USER:$USER $destPath/.tasksCode/*
+sudo chmod 755 $destPath/.tasksCode/*
+sudo chown $USER:$USER $destPath/.tasksCode/*
 
-&& mkdir /home/$USER/.tasks
-&& sudo wget -O $destPath/.help_tasksHelper  link to help file 
-&& rm /home/$USER/tasksHelper_installer*
+mkdir /home/$USER/.tasks
+sudo wget -O $destPath/.help_tasksHelper  link to help file 
+rm /home/$USER/tasksHelper_installer*
